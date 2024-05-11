cd5b804284f2ca345acba8b2352c2c84d4eb0698
Yuan Cheng <ycheng@atlassian.com>
2024-05-12 09:53:38 +1000

    Add component scan package list

--- src/main/java/com/spring/helloworld/HelloWorldSpringApplication.java
+++ src/main/java/com/spring/helloworld/HelloWorldSpringApplication.java
@@ -3,7 +3,10 @@ package com.spring.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// we can explicitly list packages we want Spring to scan, when they located outside the Main class
+@SpringBootApplication(
+    scanBasePackages = {"com.spring.helloworld"}
+)
 public class HelloWorldSpringApplication {
 
     public static void main(String[] args) {
